fb1a1c26aac0c124315ab826dcae9633f0bd9c02
Bruce Muthomi Guantai <muthomiguantai@gmail.com>
2020-01-19 09:28:54 +0300

    first commit

---
+++ AwesomeProject/src/Instaclone.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text, StyeSheet, Image} from 'react-native';
+
+class Instaclone extends Component {
+  render() {
+    return (
+      <View>
+        <Text>My Instagram Clone</Text>
+      </View>
+    );
+  }
+}
+
+export default Instaclone;
